1098265393650c69b2d29ee377a6a7aa25fd3700
jtbutler <jtbutler@iastate.edu>
2023-11-11 13:35:53 -0600

    reading form score

--- game.py
+++ game.py
@@ -1,5 +1,12 @@
 import curses
 
+def parsescore(f: list):
+    ret: str
+    name:str = list[0]
+    score:str = list[1]
+    ret = '' + score + ' -- ' + name
+    return ret
+
 def main():
 
     stdscr = curses.initscr()
@@ -8,10 +15,13 @@ def main():
     stdscr.keypad(True)
     stdscr.border()
 
+    score = open('highscore.txt', 'r')
+    highscore = score.readlines()
+
     stdscr.addstr(0, 0, str(curses.has_colors()))
     stdscr.addstr(2, 4, str(curses.has_extended_color_support()))
     stdscr.addstr(3, 6, '[s] Start Game')
-    stdscr.addstr(4, 6, '[h] High Score: ')
+    stdscr.addstr(4, 6, '[h] High Score: ' + highscore[1].strip() + ' -- ' + highscore[0].strip())
     stdscr.addstr(5, 6, '[i] inbetween')
 
     stdscr.refresh()
