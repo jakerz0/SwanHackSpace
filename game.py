fb0ca69a030052de89f0177f1901a3adecf1f0d1
jtbutler <jtbutler@iastate.edu>
2023-11-11 13:37:05 -0600

    final

--- game.py
+++ game.py
@@ -18,11 +18,13 @@ def main():
     score = open('highscore.txt', 'r')
     highscore = score.readlines()
 
-    stdscr.addstr(0, 0, str(curses.has_colors()))
-    stdscr.addstr(2, 4, str(curses.has_extended_color_support()))
+    # stdscr.addstr(0, 0, str(curses.has_colors()))
+    # stdscr.addstr(2, 4, str(curses.has_extended_color_support()))
     stdscr.addstr(3, 6, '[s] Start Game')
     stdscr.addstr(4, 6, '[h] High Score: ' + highscore[1].strip() + ' -- ' + highscore[0].strip())
     stdscr.addstr(5, 6, '[i] inbetween')
+
+    
 
     stdscr.refresh()
     stdscr.getkey()
